4d5b9beb46dae4fd1329df80399493d6af0a50ce
arcangeloBiel <joaoarcangelo7@hotmail.com>
2021-05-22 04:30:27 -0300

    create

---
+++ Api_curso/Api_curso/Controllers/PersonController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace Api_curso.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase {
+       
+
+        public PersonController(ILogger<PersonController> logger) {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber) {
+            return BadRequest("Invalid Input");
+        }
+
+      
+       
+
+    }
+}
---
+++ Api_curso/Api_curso/Model/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_curso.Model {
+    public class Person {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+
+        public string gender { get; set; }
+    }
+}
